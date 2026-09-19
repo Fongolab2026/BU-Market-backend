c34198e425bce2fadd9b612cfdf64ed73fb193c2
Joe <primsjoe0@gmail.com>
2026-09-19 13:20:03 +0200

    Add files via upload

---
+++ virtual_market/products/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from .models import Products, Categories
+from rest_framework import viewsets
+from .serializers import ProductsSerializer, CategoriesSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from users.permisions import IsAdmin,IsBuyer,IsSeller,IsSuperAdmin
+
+# Create your views here.
+class ProductPagination(PageNumberPagination):
+    page_size = 20
+
+class ProductsView(viewsets.ModelViewSet):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    pagination_class = ProductPagination
+    # ordering = "name"
+
+    def perform_create(self, serializer):
+        serializer.save(
+            owner=self.request.user
+        )
+        return serializer
+
+    def get_permissions(self):
+        if self.action == 'destroy':
+            permission_classes = [IsSeller, IsAdmin, IsSuperAdmin]
+
+        elif self.action == 'update':
+            permission_classes = [IsSeller, IsAdmin, IsSuperAdmin]
+
+        else:
+            permission_classes = [
+                IsAuthenticated
+            ]
+        return [
+            permission()
+            for permission in permission_classes    
+
+        ]
+            
+
+    
+        
+
+    
+
+class CategoriesView(viewsets.ModelViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+
+
---
+++ virtual_market/users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from .views import UserViewset
+from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = routers.DefaultRouter()
+router.register('user', UserViewset, basename='user')
+
+urlpatterns = [
+    path('users/', include(router.urls)),
+    path("login/", TokenObtainPairView.as_view(), name="token_access"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh")
+]
